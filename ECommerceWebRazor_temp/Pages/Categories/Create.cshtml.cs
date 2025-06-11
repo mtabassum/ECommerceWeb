481569d6f0128f034349e267bbccb3f2c4094c81
mtabassum <mtabassum015@gmail.com>
2025-06-11 10:41:30 -0500

    Add UI for creating categories

---
+++ ECommerceWebRazor_temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using ECommerceWebRazor_temp.Data;
+using ECommerceWebRazor_temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerceWebRazor_temp.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category{ get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
